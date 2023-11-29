d332a7fb4f6cd6178b3247922a6eca60ad5d80dd
Krittin Thirasak <krittin.thirasak@gmail.com>
2023-11-29 15:59:37 +0700

    Update Video, Ads, Comment

--- sql/youtube_create.sql
+++ sql/youtube_create.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS GoogleAccount (
     recoveryEmail 		varchar(100),
     gender 				char(1)			NOT NULL,
     CONSTRAINT chk_gender CHECK (gender IN ('M', 'F')),
-    CONSTRAINT fk_GoogleAcct_Viewer FOREIGN KEY (ID) REFERENCES Viewer(ID)
+    CONSTRAINT fk_GoogleAcct_viewerID FOREIGN KEY (ID) REFERENCES Viewer(ID)
 );
 
 CREATE TABLE IF NOT EXISTS GoogleAcct_billingInfo (
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS GoogleAcct_billingInfo (
     googleAcct_cardType 	varchar(20),
     googleAcct_cardNumber 	varchar(16),
 	CONSTRAINT pk_GAbillingInfo PRIMARY KEY (googleAcctID, googleAcct_cardType, googleAcct_cardNumber),
-	CONSTRAINT fk_GAbillingInfo_GoogleAcct FOREIGN KEY (googleAcctID) REFERENCES GoogleAccount(ID)
+	CONSTRAINT fk_GAbillingInfo_GoogleAcctID FOREIGN KEY (googleAcctID) REFERENCES GoogleAccount(ID)
 );
 
 CREATE TABLE IF NOT EXISTS GoogleAcct_address (
@@ -38,21 +38,21 @@ CREATE TABLE IF NOT EXISTS GoogleAcct_address (
     googleAcct_province 		varchar(50),
     googleAcct_road 			varchar(100),
     CONSTRAINT pk_GAaddress PRIMARY KEY (googleAcctID, googleAcct_addressNumber, googleAcct_postCode, googleAcct_district, googleAcct_province, googleAcct_road),
-	CONSTRAINT fk_GAaddress_GoogleAcct FOREIGN KEY (googleAcctID) REFERENCES GoogleAccount(ID)
+	CONSTRAINT fk_GAaddress_GoogleAcctID FOREIGN KEY (googleAcctID) REFERENCES GoogleAccount(ID)
 );
 
 -- Google Ads Account 
 CREATE TABLE IF NOT EXISTS GoogleAdsAccount (
     googleAdsAcctID varchar(128) 	PRIMARY KEY,
     googleAcctID 	varchar(128) 	NOT NULL,
-    CONSTRAINT fk_GoogleAdsAcct_GoogleAcct FOREIGN KEY (googleAcctID) REFERENCES GoogleAccount(ID)
+    CONSTRAINT fk_GoogleAdsAcct_GoogleAcctID FOREIGN KEY (googleAcctID) REFERENCES GoogleAccount(ID)
 );
 
 CREATE TABLE IF NOT EXISTS GoogleAdsAcct_linkedAccount (
     googleAdsAcctID 			varchar(128),
     googleAdsAcct_linkedAccount varchar(125) NOT NULL,
     PRIMARY KEY (googleAdsAcctID, googleAdsAcct_linkedAccount),
-    CONSTRAINT fk_GAAlinkedAcct_GoogleAdsAcct FOREIGN KEY (googleAdsAcctID) REFERENCES GoogleAdsAccount(googleAdsAcctID)
+    CONSTRAINT fk_GAAlinkedAcct_GoogleAdsAcctID FOREIGN KEY (googleAdsAcctID) REFERENCES GoogleAdsAccount(googleAdsAcctID)
 );
 
 -- Channel's Creator
@@ -75,17 +75,17 @@ CREATE TABLE IF NOT EXISTS ChannelCreator (
     uploadPlaylist 		VARCHAR(128),
     thumbnails 			VARCHAR(128)	NOT NULL,
     googleAcctID 		VARCHAR(128)	NOT NULL,
-    CONSTRAINT fk_ChannelCreator_Viewer FOREIGN KEY (ID) REFERENCES Viewer(ID),
-	CONSTRAINT fk_ChannelCreator_GoogleAcct FOREIGN KEY (googleAcctID) REFERENCES GoogleAccount(ID)
+    CONSTRAINT fk_ChannelCreator_ViewerID FOREIGN KEY (ID) REFERENCES Viewer(ID),
+	CONSTRAINT fk_ChannelCreator_GoogleAcctID FOREIGN KEY (googleAcctID) REFERENCES GoogleAccount(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Subscribe (
     channelID 			VARCHAR(128),
     subscriberID 		VARCHAR(128),
-    subscriptionDate 	DATE 			NOT NULL,
+    subscriptionDate 	DATETIME 		NOT NULL,
     CONSTRAINT pk_Subscribe PRIMARY KEY (channelID, subscriberID),
-    CONSTRAINT fk_Subscribe_channel FOREIGN KEY (channelID) REFERENCES ChannelCreator(ID),
-    CONSTRAINT fk_Subscribe_subscriber FOREIGN KEY (subscriberID) REFERENCES ChannelCreator(ID)
+    CONSTRAINT fk_Subscribe_channelID FOREIGN KEY (channelID) REFERENCES ChannelCreator(ID),
+    CONSTRAINT fk_Subscribe_subscriberID FOREIGN KEY (subscriberID) REFERENCES ChannelCreator(ID)
 );
 
 -- YPP & Monetization
@@ -94,44 +94,44 @@ CREATE TABLE IF NOT EXISTS YPP (
     YPPID 		VARCHAR(128),
     acceptedAt 	DATETIME	NOT NULL,
 	CONSTRAINT pk_YPP PRIMARY KEY (channelID, YPPID),
-    CONSTRAINT fk_YPP_channel FOREIGN KEY (channelID) REFERENCES ChannelCreator(ID)
+    CONSTRAINT fk_YPP_channelID FOREIGN KEY (channelID) REFERENCES ChannelCreator(ID)
 ); 
 
-CREATE TABLE MonetizationFeature (
+CREATE TABLE IF NOT EXISTS MonetizationFeature (
     featureName		VARCHAR(128)	PRIMARY KEY,
     description 	VARCHAR(1000)	NOT NULL
 );
 
-CREATE TABLE MntFeature_requirement (
+CREATE TABLE IF NOT EXISTS MntFeature_requirement (
     featureName 		VARCHAR(128) 	NOT NULL,
     mntFeature_requirement 	VARCHAR(500)	NOT NULL,
-    CONSTRAINT pk_MntFeatureRequirement PRIMARY KEY (featureName, mntFeature_requirement),
-    CONSTRAINT fk_MntFeatureRequirement_featureName FOREIGN KEY (featureName) REFERENCES MonetizationFeature(featureName)
+    CONSTRAINT pk_MFrequirement PRIMARY KEY (featureName, mntFeature_requirement),
+    CONSTRAINT fk_MFrequirement_featureName FOREIGN KEY (featureName) REFERENCES MonetizationFeature(featureName)
 );
 
-CREATE TABLE Manage_MntFeature (
+CREATE TABLE IF NOT EXISTS Manage_MntFeature (
     YPP_channelID 			VARCHAR(128)	NOT NULL,
     YPP_YPPID 				VARCHAR(128) 	NOT NULL,
     mntFeature_featureName	VARCHAR(128)	NOT NULL,
     isEnable				BOOLEAN			NOT NULL,
-    CONSTRAINT pk_ManageMntFeature PRIMARY KEY (YPP_channelID, YPP_YPPID, mntFeature_featureName),
-    CONSTRAINT fk_ManageMntFeature_YPP FOREIGN KEY (YPP_channelID, YPP_YPPID) REFERENCES YPP(channelID, YPPID),
-    CONSTRAINT fk_ManageMntFeature_featureName FOREIGN KEY (mntFeature_featureName) REFERENCES MonetizationFeature(featureName)
+    CONSTRAINT pk_ManageMF PRIMARY KEY (YPP_channelID, YPP_YPPID, mntFeature_featureName),
+    CONSTRAINT fk_ManageMF_YPP FOREIGN KEY (YPP_channelID, YPP_YPPID) REFERENCES YPP(channelID, YPPID),
+    CONSTRAINT fk_ManageMF_featureName FOREIGN KEY (mntFeature_featureName) REFERENCES MonetizationFeature(featureName)
 );
 
-CREATE TABLE RevenueReceivingMethod (
+CREATE TABLE IF NOT EXISTS RevenueReceivingMethod (
     methodName 		VARCHAR(128) 	PRIMARY KEY,
     isThirdParty 	BOOLEAN 		NOT NULL
 );
 
-CREATE TABLE RRMethod_paymentOption (
+CREATE TABLE IF NOT EXISTS RRMethod_paymentOption (
     methodName 				VARCHAR(128) 	NOT NULL,
     RRMethod_paymentOption 	VARCHAR(50)		NOT NULL,
-    CONSTRAINT pk_RRMethodPaymentOption PRIMARY KEY (methodName, RRMethod_paymentOption),
-    CONSTRAINT fk_RRMethodPaymentOption_methodName FOREIGN KEY (methodName) REFERENCES RevenueReceivingMethod(methodName)
+    CONSTRAINT pk_RRMpaymentOption PRIMARY KEY (methodName, RRMethod_paymentOption),
+    CONSTRAINT fk_RRMpaymentOption_methodName FOREIGN KEY (methodName) REFERENCES RevenueReceivingMethod(methodName)
 );
 
-CREATE TABLE Choose_RRMethod (
+CREATE TABLE IF NOT EXISTS Choose_RRMethod (
     YPP_channelID 		VARCHAR(128)	NOT NULL,
     YPP_YPPID 			VARCHAR(128) 	NOT NULL,
     RRMethod_methodName VARCHAR(128)	NOT NULL,
@@ -140,14 +140,13 @@ CREATE TABLE Choose_RRMethod (
     district 			VARCHAR(100)	NOT NULL,
     province 			VARCHAR(100)	NOT NULL,
     postalCode 			VARCHAR(10)		NOT NULL,
-    CONSTRAINT pk_ChooseRRMethod PRIMARY KEY (YPP_channelID, YPP_YPPID, RRMethod_paymentOption),
-    CONSTRAINT fk_ChooseRRMethod_YPP FOREIGN KEY (YPP_channelID, YPP_YPPID) REFERENCES YPP(channelID, YPPID),
-	CONSTRAINT fk_ChooseRRMethod_methodName FOREIGN KEY (RRMMethodName) REFERENCES RevenueReceivingMethod(methodName)
+    CONSTRAINT pk_ChooseRRM PRIMARY KEY (YPP_channelID, YPP_YPPID, RRMethod_methodName),
+    CONSTRAINT fk_ChooseRRM_YPP FOREIGN KEY (YPP_channelID, YPP_YPPID) REFERENCES YPP(channelID, YPPID),
+	CONSTRAINT fk_ChooseRRM_methodName FOREIGN KEY (RRMethod_methodName) REFERENCES RevenueReceivingMethod(methodName)
 );
 
-
 -- Video
-CREATE TABLE Video (
+CREATE TABLE IF NOT EXISTS Video (
     videoID 		VARCHAR(128)	PRIMARY KEY,
     publishedAt 	DATETIME		NOT NULL,
     title 			VARCHAR(50)		NOT NULL,
@@ -159,114 +158,113 @@ CREATE TABLE Video (
     uploadStatus 	VARCHAR(20)		NOT NULL,
     visibility 		VARCHAR(20)		NOT NULL,
     fileDetails 	VARCHAR(128)	NOT NULL,
-    suggestions 	VARCHAR(128)	NOT NULL,
-    localizations 	VARCHAR(128)	NOT NULL,
+    license			VARCHAR(50)		NOT NULL,
+    madeForKids 	BOOLEAN			NOT NULL,
     channelID 		VARCHAR(128)	NOT NULL,
-    CONSTRAINT fk_Video_channel FOREIGN KEY (channelID) REFERENCES ChannelCreator(ID)
+    CONSTRAINT fk_Video_channelID FOREIGN KEY (channelID) REFERENCES ChannelCreator(ID)
 );
 
-CREATE TABLE IF NOT EXISTS Video_tags (
+CREATE TABLE IF NOT EXISTS Video_tag (
     videoID 	VARCHAR(128),
-    video_tags 	VARCHAR(128),
-    CONSTRAINT pk_Vtags PRIMARY KEY (videoID, video_tags),
-    CONSTRAINT pk_Vtags_Video FOREIGN KEY (videoID) REFERENCES Video(videoID)
+    video_tag 	VARCHAR(128)	NOT NULL,
+    CONSTRAINT pk_Vtag PRIMARY KEY (videoID, video_tag),
+    CONSTRAINT pk_Vtag_videoID FOREIGN KEY (videoID) REFERENCES Video(videoID)
 );
 
-CREATE TABLE Members_only_Video (
-    videoID VARCHAR(128) PRIMARY KEY,
-    memberLevel VARCHAR(20),
-    channelID VARCHAR(20),
-    YPPID VARCHAR(20),
-    CONSTRAINT fk_members_only_video_video FOREIGN KEY (videoID) REFERENCES Video(videoID),
-    CONSTRAINT fk_members_only_video_ypp FOREIGN KEY (channelID, YPPID) REFERENCES YPP(channelID, YPPID)
+CREATE TABLE IF NOT EXISTS MembersOnlyVideo (
+    videoID 		VARCHAR(128) 	PRIMARY KEY,
+    memberLevel 	VARCHAR(20)		NOT NULL,
+    YPP_channelID 	VARCHAR(128)	NOT NULL,
+    YPP_YPPID 		VARCHAR(128) 	NOT NULL,
+    CONSTRAINT fk_MembersOnlyVideo_videoID FOREIGN KEY (videoID) REFERENCES Video(videoID),
+    CONSTRAINT fk_MembersOnlyVideo_YPP FOREIGN KEY (YPP_channelID, YPP_YPPID) REFERENCES YPP(channelID, YPPID)
 );
 
-
-
-CREATE TABLE Monetized_Video (
-    videoID VARCHAR(128) PRIMARY KEY,
-    adsType VARCHAR(128),
-    location VARCHAR(128),
-    monetizationStatus BOOLEAN,
-    channelID VARCHAR(128),
-    YPPID VARCHAR(128),
-    CONSTRAINT fk_monetized_video_video FOREIGN KEY (videoID) REFERENCES Video(videoID),
-    CONSTRAINT fk_monetized_video_ypp FOREIGN KEY (channelID, YPPID) REFERENCES YPP(channelID, YPPID)
+CREATE TABLE IF NOT EXISTS MonetizedVideo (
+    videoID 			VARCHAR(128) 	PRIMARY KEY,
+    adsType 			VARCHAR(128)	NOT NULL,
+    location 			VARCHAR(128)	NOT NULL,
+    monetizationStatus 	BOOLEAN			NOT NULL,
+    YPP_channelID 		VARCHAR(128)	NOT NULL,
+    YPP_YPPID 			VARCHAR(128) 	NOT NULL,
+    CONSTRAINT fk_MonetizedVideo_videoID FOREIGN KEY (videoID) REFERENCES Video(videoID),
+    CONSTRAINT fk_MonetizedVideo_YPP FOREIGN KEY (YPP_channelID, YPP_YPPID) REFERENCES YPP(channelID, YPPID)
 );
 
-
-CREATE TABLE Comments (
-    commentID VARCHAR(128) PRIMARY KEY,
-    updateTime TIME,
-    publishedTime TIME,
-    moderationStatus VARCHAR(128),
-    likeCounts VARCHAR(50),
-    viewerRating VARCHAR(128),
-    rateStatus BOOLEAN,
-    textOriginal VARCHAR(128),
-    textDisplay VARCHAR(128),
-    authorChannelUrl VARCHAR(128),
-    authorProfileImageUrl VARCHAR(128),
-    authorDisplayName VARCHAR(128),
-    channelID VARCHAR(128),
-    videoID VARCHAR(128),
-    CONSTRAINT fk_comments_channelID FOREIGN KEY (channelID) REFERENCES Channel_Creator(ID)
-);
-
+-- Engage & Comment
 CREATE TABLE IF NOT EXISTS Engage (
-    viewerID varchar(128),
-    videoID varchar(128),
-    usedDevice varchar(20),
-    watchDuration time,
-    isSaved boolean,
-    isDisliked boolean,
-    isLiked boolean,
-    PRIMARY KEY (viewerID, videoID),
-    FOREIGN KEY (viewerID) REFERENCES Viewer(ID),
-    FOREIGN KEY (videoID) REFERENCES Video(videoID)
+    viewerID 		VARCHAR(128),
+    videoID 		VARCHAR(128),
+    usedDevice 		VARCHAR(20)	NOT NULL,
+    watchDuration	TIME		NOT NULL,
+    isSaved 		BOOLEAN		NOT NULL,
+    isDisliked 		BOOLEAN		NOT NULL,
+    isLiked 		BOOLEAN		NOT NULL,
+    CONSTRAINT pk_Engage PRIMARY KEY (viewerID, videoID),
+    CONSTRAINT fk_Engage_viewerID FOREIGN KEY (viewerID) REFERENCES Viewer(ID),
+	CONSTRAINT fk_Engage_videoID FOREIGN KEY (videoID) REFERENCES Video(videoID)
 );
 
-CREATE TABLE IF NOT EXISTS `Video Ads` (
-    videoAdsID VARCHAR(128) PRIMARY KEY,
-    length TIME,
-    objective VARCHAR(128),
-    videoFormatType VARCHAR(128),
-    targetLocation VARCHAR(128),
-    targetLanguage VARCHAR(128),
-    budget DECIMAL(16,2),
-    googleAdsID VARCHAR(128),
-    FOREIGN KEY (googleAdsID) REFERENCES GoogleAdsAccount(googleAdsAccountID)
+CREATE TABLE IF NOT EXISTS Comment (
+    commentID 				VARCHAR(128)	PRIMARY KEY,
+    publishedAt 			DATETIME		NOT NULL,
+    updatedAt 				DATETIME		NOT NULL,
+    moderationStatus 		VARCHAR(50)		NOT NULL,
+    likeCounts 				INT				NOT NULL,
+    isPinned 				BOOLEAN			NOT NULL,
+    canRate 				BOOLEAN			NOT NULL,
+    textOriginal 			VARCHAR(5000)	NOT NULL,
+    textDisplay 			VARCHAR(10000)	NOT NULL,
+    authorChannelUrl 		VARCHAR(128)	NOT NULL,
+    authorProfileImageUrl	VARCHAR(128)	NOT NULL,
+    authorDisplayName 		VARCHAR(128)	NOT NULL,
+    channelID 				VARCHAR(128)	NOT NULL,
+    videoID 				VARCHAR(128)	NOT NULL,
+    CONSTRAINT fk_Comment_channelID FOREIGN KEY (channelID) REFERENCES ChannelCreator(ID),
+    CONSTRAINT fk_Comment_videoID FOREIGN KEY (videoID) REFERENCES Video(videoID)
 );
 
-CREATE TABLE IF NOT EXISTS `Appear_Ads` (
-    videoID VARCHAR(128),
-    videoAdsID VARCHAR(128),
-    videoAdsViews INT,
-    PRIMARY KEY (videoID, videoAdsID),
-    FOREIGN KEY (videoID) REFERENCES `Video`(videoID),
-    FOREIGN KEY (videoAdsID) REFERENCES `Video Ads`(videoAdsID)
+-- Video Ads
+CREATE TABLE IF NOT EXISTS VideoAds (
+    videoAdsID		VARCHAR(128)	PRIMARY KEY,
+    length			TIME			NOT NULL,
+    objective		VARCHAR(128)	NOT NULL,
+    videoFormatType	VARCHAR(50)		NOT NULL,
+    targetLocation 	VARCHAR(128)	NOT NULL,
+    targetLanguage 	VARCHAR(128)	NOT NULL,
+    budget 			DECIMAL(16,2)	NOT NULL,
+    googleAdsAcctID	VARCHAR(128)	NOT NULL,
+    CONSTRAINT fk_VideoAds_GoogleAdsAcctID FOREIGN KEY (googleAdsAcctID) REFERENCES GoogleAdsAccount(googleAdsAcctID)
 );
 
-
-CREATE TABLE IF NOT EXISTS `VideoAds_topics` (
-    videoAdsID VARCHAR(128),
-    VIDEOADStopics VARCHAR(128),
-    URLlinkofVideoAds VARCHAR(255),
-    PRIMARY KEY (videoAdsID, VIDEOADStopics),
-    FOREIGN KEY (videoAdsID) REFERENCES `Video Ads`(videoAdsID)
+CREATE TABLE IF NOT EXISTS VideoAds_topic (
+    videoAdsID 		VARCHAR(128),
+    videoAds_topic 	VARCHAR(128),
+    videoAds_url 	VARCHAR(128),
+    CONSTRAINT pk_VAtopic PRIMARY KEY (videoAdsID, videoAds_topic, videoAds_url),
+    CONSTRAINT fk_VAtopic_videoAdsID FOREIGN KEY (videoAdsID) REFERENCES VideoAds(videoAdsID)
 );
 
-CREATE TABLE IF NOT EXISTS `VideoAds_placements` (
-    videoAdsID VARCHAR(128),
-    VIDEOADSplacements TIME,
-    PRIMARY KEY (videoAdsID, VIDEOADSplacements),
-    FOREIGN KEY (videoAdsID) REFERENCES `Video Ads`(videoAdsID)
+CREATE TABLE IF NOT EXISTS VideoAds_placement (
+    videoAdsID			VARCHAR(128),
+    videoAds_placement	TIME,
+    CONSTRAINT pk_VAplacement PRIMARY KEY (videoAdsID, videoAds_placement),
+    CONSTRAINT fk_VAplacement_videoAdsID FOREIGN KEY (videoAdsID) REFERENCES VideoAds(videoAdsID)
 );
 
-CREATE TABLE IF NOT EXISTS `VideoAds_keywords` (
-    videoAdsID VARCHAR(128),
-    VIDEOADSkeywords VARCHAR(128),
-    times INT,
-    PRIMARY KEY (videoAdsID, VIDEOADSkeywords),
-    FOREIGN KEY (videoAdsID) REFERENCES `Video Ads`(videoAdsID)
+CREATE TABLE IF NOT EXISTS VideoAds_keyword (
+    videoAdsID 				VARCHAR(128),
+    videoAds_keyword 		VARCHAR(128),
+    videoAds_keywordTimes	INT,
+    CONSTRAINT pk_VAkeyword PRIMARY KEY (videoAdsID, videoAds_keyword, videoAds_keywordTimes),
+    CONSTRAINT fk_VAkeyword_videoAdsID FOREIGN KEY (videoAdsID) REFERENCES VideoAds(videoAdsID)
+);
+
+CREATE TABLE IF NOT EXISTS Appear_VideoAds (
+    videoID 		VARCHAR(128),
+    videoAdsID 		VARCHAR(128),
+    videoAdsViews	INT,
+	CONSTRAINT pk_AppearVA PRIMARY KEY (videoID, videoAdsID),
+    CONSTRAINT fk_AppearVA_videoID FOREIGN KEY (videoID) REFERENCES Video(videoID),
+    CONSTRAINT fk_AppearVA_videoAdsID FOREIGN KEY (videoAdsID) REFERENCES VideoAds(videoAdsID)
 );
